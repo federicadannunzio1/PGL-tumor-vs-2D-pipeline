9b315edf25b7929c38cbbb3340abe0a466af6387
Federica D'Annunzio <federica.dannunzio1@gmail.com>
2026-09-16 13:56:40 +0200

    Restore job1 mem to 120G on dss (dss supports large-memory nodes)

--- pipeline_linee_vs_tumore/submit/job1_scrna_reference.sh
+++ pipeline_linee_vs_tumore/submit/job1_scrna_reference.sh
@@ -12,7 +12,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=55G
+#SBATCH --mem=120G
 #SBATCH --time=10:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=federica.dannunzio@uniroma1.it
@@ -21,7 +21,7 @@ echo "================================================"
 echo "Job:   $SLURM_JOB_ID - 01_scrna_reference.R"
 echo "Node:  $SLURMD_NODENAME"
 echo "Start: $(date)"
-echo "Mem:   55G | CPUs: $SLURM_CPUS_PER_TASK"
+echo "Mem:   120G | CPUs: $SLURM_CPUS_PER_TASK"
 echo "================================================"
 
 source /lustre/software/anaconda/2022.10_all/etc/profile.d/conda.sh
